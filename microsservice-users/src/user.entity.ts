7defe908534662535a56f7c80f479783b38426fd
acristoni <adonai_cn@hotmail.com>
2023-09-05 07:46:50 -0300

    FEAT: user entity

---
+++ microsservice-users/src/user.entity.ts
@@ -0,0 +1,37 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('user')
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar' })
+  first_name: string;
+
+  @Column({ type: 'varchar' })
+  last_name: string;
+
+  @Column({ unique: true, length: 11 })
+  document: string;
+
+  @Column({ type: 'varchar' })
+  email: string;
+
+  @Column({ type: 'varchar' })
+  phone_number: string;
+
+  @Column({ type: 'date' })
+  birth_date: Date;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
