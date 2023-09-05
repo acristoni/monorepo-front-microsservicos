d64d09062925be3178ea8ebf551595485e7dc15a
acristoni <adonai_cn@hotmail.com>
2023-09-05 07:19:22 -0300

    BUILD: microsservico usuario & swagger

---
+++ microsservice-users/src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Usuário')
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @ApiOperation({
+    summary: 'Apenas para teste de funcionamento da api',
+  })
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
---
+++ microsservice-users/src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Microsserviço-Usuário')
+    .setDescription(
+      'Microsserviço para criação, atualização e autenticação de usuário',
+    )
+    .setVersion('1.0')
+    .addTag('Usuário')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const PORT = process.env.PORT || 3003;
+  await app.listen(PORT);
+}
+bootstrap();
